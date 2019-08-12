84787390c0ea0355cf85a409ef1d51b08a3dbcd1
Kazuki Aizawa <kaz130@users.noreply.github.com>
2019-08-12 23:57:50 +0900

    Add create book method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,24 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
+  end
+
+  def create
+    @book = current_user.books.new(book_params)
+    if @book.save
+      redirect_to @book, notice: "単語帳「#{@book.name}」を登録しました．"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :description)
   end
 end
